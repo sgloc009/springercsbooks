6532cfc2d48275a7855e4095604814599c0af70e
sgloc009 <rickirod007@gmail.com>
2020-04-30 17:03:15 +0530

    Add files via upload

--- springer_books.py
+++ springer_books.py
@@ -9,20 +9,18 @@ books_link = [i.attrs['href'] for i in med_html.find_all('a') if i.attrs['href']
 direct_download_links = []
 print(len(books_link))
 for i in range(len(books_link)):
-    
     pdf_writer = PyPDF2.PdfFileWriter()
     springer_res = requests.get(books_link[i])
-    print(springer_res)
     springer_html = BeautifulSoup((springer_res.text),'html.parser')
+    name = springer_html.find('h1').text
     link = 'https://link.springer.com'+springer_html.find_all(attrs= {'class':'test-bookpdf-link'})[0].attrs['href']
     direct_download_links.append(link)
-    print(link);
     response = http.request('GET', link).data
     with io.BytesIO(response) as curr_pdf:
         read_pdf = PyPDF2.PdfFileReader(curr_pdf)
         num_pages = read_pdf.getNumPages()
         for j in range(num_pages):
             pdf_writer.addPage(read_pdf.getPage(j))
-        with open('document'+str(i)+'.pdf','wb') as out:
+        with open(name+'.pdf','wb') as out:
             pdf_writer.write(out)
             out.close();
